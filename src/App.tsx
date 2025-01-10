efca031f1ff3212b465010c8f4a3370f9fd33bec
hobermin <sonhomin98@naver.com>
2025-01-10 17:54:12 +0900

    init: 프로젝트 시작

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./index.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button className="rounded-full bg-blue-200">shadcn Button</Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="border text-2xl	">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
