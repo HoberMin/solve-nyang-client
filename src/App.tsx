036482605e3951099dacde77a228244af1bca8c7
HoberMin <102784200+HoberMin@users.noreply.github.com>
2025-01-12 16:25:38 +0900

    chore: eslint , prettier 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import "./index.css";
-import { Button } from "./components/ui/button";
+import { Button } from './components/ui/button';
+import './index.css';
 
-function App() {
+const App = () => {
   // const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
-        <Button className="rounded-full bg-blue-200">shadcn Button</Button>
+      <div className='card'>
+        <Button className='rounded-full bg-blue-200'>shadcn Button</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="border text-2xl	">
+      <p className='border text-2xl'>
         Click on the Vite and React logos to learn more
       </p>
     </>
   );
-}
+};
 
 export default App;
