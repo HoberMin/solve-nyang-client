ad07f85fb057722de666b94e3ad0d8312bc86f6c
HoberMin <102784200+HoberMin@users.noreply.github.com>
2025-01-13 22:58:52 +0900

    feat: Index 페이지 개발

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,23 @@
-// RootLayout.jsx
+// Layout.tsx
 import { PropsWithChildren, Suspense } from 'react';
 
 import Header from './Header';
 import RetroLoading from './RetroLoading';
 
-const RootLayout = ({ children }: PropsWithChildren) => {
+const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className='min-h-screen w-screen bg-[#111827]'>
-      <Header />
-      <main className='h-[calc(100vh-3.5rem)] w-full'>
+    <div className='relative min-h-screen w-screen bg-[#111827]'>
+      {/* 헤더를 fixed로 변경하고 z-index 추가 */}
+      <div className='fixed top-0 z-50 w-full'>
+        <Header />
+      </div>
+
+      {/* main 영역에 상단 패딩 추가 */}
+      <main className='h-screen w-full pt-14'>
         <Suspense fallback={<RetroLoading />}>{children}</Suspense>
       </main>
     </div>
   );
 };
 
-export default RootLayout;
+export default Layout;
--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -1,11 +1,16 @@
 // src/router.tsx
 import { createBrowserRouter } from 'react-router-dom';
 
+import Index from '@/pages/index';
 import Commingsoon from '@/components/Commingsoon';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Commingsoon />,
+  },
+  {
+    path: '/index',
+    element: <Index />,
   },
 ]);
