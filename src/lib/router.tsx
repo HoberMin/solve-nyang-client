5e2296be260a5b2b10e4c70b95f33b3a85bc0bf8
hobermin <sonhomin98@naver.com>
2025-01-15 17:22:50 +0900

    feat: NotFoundPage

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -1,9 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Commingsoon from '@/components/Commingsoon';
+import NotFoundPage from '@/pages/NotFoundPage';
 import SalePage from '@/pages/SalePage';
 import UserPage from '@/pages/UserPage';
 import Index from '@/pages/index';
+
+// 방금 만든 404 컴포넌트를 import 합니다
 
 export const router = createBrowserRouter([
   {
@@ -21,5 +24,9 @@ export const router = createBrowserRouter([
   {
     path: '/sale',
     element: <SalePage />,
+  },
+  {
+    path: '*', // 위의 모든 경로와 매칭되지 않는 경우 404 페이지로 이동
+    element: <NotFoundPage />,
   },
 ]);
