dc1567a37a42e01e216d3b99b8d9bdf1619076d1
hobermin <sonhomin98@naver.com>
2025-01-15 14:18:54 +0900

    feat: salePage 화면 구현현

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Commingsoon from '@/components/Commingsoon';
+import SalePage from '@/pages/SalePage';
 import UserPage from '@/pages/UserPage';
 import Index from '@/pages/index';
 
@@ -16,5 +17,9 @@ export const router = createBrowserRouter([
   {
     path: '/profile',
     element: <UserPage />,
+  },
+  {
+    path: '/sale',
+    element: <SalePage />,
   },
 ]);
