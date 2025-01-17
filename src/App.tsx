94c31d9e5318977df54a89e858280b8a3bf74ac6
hobermin <sonhomin98@naver.com>
2025-01-17 13:56:31 +0900

    refactor: Errorboundary 걷어내기기

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
-import {
-  QueryClientProvider,
-  QueryErrorResetBoundary,
-} from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ErrorBoundary } from 'react-error-boundary';
 import { RouterProvider } from 'react-router-dom';
 import { Toaster } from 'sonner';
 
-import RetroError from './components/ErrorBoundary';
 import { queryClient } from './lib/queryClient';
 import { router } from './lib/router';
 
 const App = () => {
   return (
-    <QueryErrorResetBoundary>
-      {({ reset }) => (
-        <ErrorBoundary onReset={reset} FallbackComponent={RetroError}>
-          <QueryClientProvider client={queryClient}>
-            <Toaster />
-            <RouterProvider router={router} />
-            <ReactQueryDevtools initialIsOpen={false} />
-          </QueryClientProvider>
-        </ErrorBoundary>
-      )}
-    </QueryErrorResetBoundary>
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
