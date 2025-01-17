3b5d10d675b546904dd801dca416ca941997c3f0
hobermin <sonhomin98@naver.com>
2025-01-17 13:50:55 +0900

    refactor: 레이아웃 구조 변경경

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-// src/App.tsx
-import { Suspense } from 'react';
-
-import { QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryClientProvider,
+  QueryErrorResetBoundary,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ErrorBoundary } from 'react-error-boundary';
 import { RouterProvider } from 'react-router-dom';
 import { Toaster } from 'sonner';
 
+import RetroError from './components/ErrorBoundary';
 import { queryClient } from './lib/queryClient';
 import { router } from './lib/router';
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Toaster />
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </Suspense>
-    </QueryClientProvider>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary onReset={reset} FallbackComponent={RetroError}>
+          <QueryClientProvider client={queryClient}>
+            <Toaster />
+            <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
   );
 };
 
--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -1,32 +1,36 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Commingsoon from '@/components/Commingsoon';
+import RetroError from '@/components/ErrorBoundary';
 import NotFoundPage from '@/pages/NotFoundPage';
 import SalePage from '@/pages/SalePage';
 import UserPage from '@/pages/UserPage';
 import Index from '@/pages/index';
 
-// 방금 만든 404 컴포넌트를 import 합니다
-
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Commingsoon />,
-  },
-  {
-    path: '/service',
-    element: <Index />,
-  },
-  {
-    path: '/profile',
-    element: <UserPage />,
-  },
-  {
-    path: '/sale',
-    element: <SalePage />,
-  },
-  {
-    path: '*', // 위의 모든 경로와 매칭되지 않는 경우 404 페이지로 이동
-    element: <NotFoundPage />,
+    errorElement: <RetroError />,
+    children: [
+      {
+        path: '/',
+        element: <Commingsoon />,
+      },
+      {
+        path: '/service',
+        element: <Index />,
+      },
+      {
+        path: '/profile',
+        element: <UserPage />,
+      },
+      {
+        path: '/sale',
+        element: <SalePage />,
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
+      },
+    ],
   },
 ]);
