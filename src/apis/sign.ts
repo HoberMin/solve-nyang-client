b4d6068b9155cdf95976813a722c204242324c7e
zoe <ddu_buk_e@naver.com>
2025-01-18 23:38:22 +0900

    feat: sign api axios 수정

--- src/apis/sign.ts
+++ src/apis/sign.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import { domain } from './avatar';
 
 interface SignInRequest {
@@ -5,26 +7,23 @@ interface SignInRequest {
   password: string;
 }
 
+const axiosInstance = axios.create({
+  baseURL: domain,
+  timeout: 5000, // 5초 후 자동으로 요청 중단
+  withCredentials: true, // CORS 요청에서 쿠키를 포함하기 위해 필요
+  headers: {
+    'Contenet-Type': 'application/json',
+  },
+});
+
 export const signIn = async ({ nickname, password }: SignInRequest) =>
-  await fetch(`${domain}/account/signin`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      nickname,
-      password,
-    }),
+  await axiosInstance.post('/account/signin', {
+    nickname,
+    password,
   });
 
 export const signUp = async ({ nickname, password }: SignInRequest) =>
-  await fetch(`${domain}/account/signup`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      nickname,
-      password,
-    }),
+  await axiosInstance.post('/account/signup', {
+    nickname,
+    password,
   });
