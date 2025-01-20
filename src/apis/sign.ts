1b2d3f1b81a006bc907bd97c93f8a584b79e9909
Zoe <ddu_buk_e@naver.com>
2025-01-20 18:01:43 +0900

    feat: 에러 처리 로직 추가

--- src/apis/sign.ts
+++ src/apis/sign.ts
@@ -15,9 +15,36 @@ const axiosInstance = axios.create({
   timeout: 5000,
   withCredentials: true,
   headers: {
-    'Contenet-Type': 'application/json',
+    'Content-Type': 'application/json',
   },
 });
+
+// 인터셉터: 모든 응담/요청을 가로채서 처리
+// 응답 인터셉터
+axiosInstance.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      localStorage.setItem('redirectPath', window.location.pathname);
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
+
+// 요청 인터셉터
+axiosInstance.interceptors.request.use(
+  config => {
+    // localStorage에서 토큰 가져오기
+    const token = localStorage.getItem('token');
+    // 토큰이 있으면 요청 헤더에 추가
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error),
+);
 
 interface SignInResponse {
   accessToken: string;
@@ -32,47 +59,22 @@ interface AxiosResponse<T> {
 }
 
 export const signIn = async (authForm: AuthRequest) =>
-  (await axiosInstance.post(
-    '/account/signin',
-    authForm,
-  )) as AxiosResponse<SignInResponse>;
+  (await axiosInstance.post('/account/signin', authForm)) as SignInResponse;
 
 export const signUp = async (authForm: AuthRequest) =>
-  (await axiosInstance.post(
-    '/account/signup',
-    authForm,
-  )) as AxiosResponse<SignUpResponse>;
+  (await axiosInstance.post('/account/signup', authForm)) as SignUpResponse;
 
 export const useSignIn = () => {
-  const navigate = useNavigate();
-
-  return useMutation({
+  const { mutateAsync } = useMutation({
     mutationFn: (authForm: AuthRequest) => signIn(authForm),
-    onSuccess: response => {
-      const { accessToken } = response.data;
-      localStorage.setItem('token', accessToken);
-      toast.success('로그인에 성공했습니다.');
-      navigate('/');
-    },
-    onError: (error: Error) => {
-      toast.error('로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.');
-      console.error(error);
-    },
   });
+
+  return mutateAsync;
 };
 
 export const useSignUp = () => {
-  const navigate = useNavigate();
-
-  const { mutate } = useMutation({
-    mutationFn: (formData: AuthRequest) => signUp(formData),
-    onSuccess: () => {
-      toast.success('회원가입이 완료되었습니다.');
-      navigate('/login');
-    },
-    onError: () => {
-      toast.error('회원가입에 실패했습니다. 다시 시도해주세요.');
-    },
+  const { mutateAsync } = useMutation({
+    mutationFn: (formData: AuthRequest) => signIn(formData),
   });
 
   return mutate;
