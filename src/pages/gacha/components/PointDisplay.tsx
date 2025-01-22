55c697e7e8b3473707209483164d9282a76806a3
ebeleey <ebeleey@gmail.com>
2025-01-22 09:38:53 +0900

    design: 폰트 크기, 버튼 UI 수정

--- src/pages/gacha/components/PointDisplay.tsx
+++ src/pages/gacha/components/PointDisplay.tsx
@@ -7,11 +7,11 @@ interface PointDisplayProps {
 export const PointDisplay = ({ point }: PointDisplayProps) => {
   return (
     <div className='w-48 rounded-lg bg-white/10 px-6 py-4 shadow-md backdrop-blur-sm'>
-      <h2 className='mb-2 text-center text-lg font-bold text-white'>
+      <h2 className='mb-2 text-center text-xl font-bold text-white'>
         내 포인트
       </h2>
       <hr className='mb-4' />
-      <div className='flex text-white'>
+      <div className='flex text-lg text-white'>
         <img src={coinImg} alt='coin' className='w-8' />
         {point.toLocaleString()}
       </div>
