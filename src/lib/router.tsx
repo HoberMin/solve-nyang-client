b8e2de5bcf86e0efd68b416e48f2c0ec08533729
HoberMin <102784200+HoberMin@users.noreply.github.com>
2025-01-26 23:17:17 +0900

    feat: 상점 탭 추가

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import RetroError from '@/components/ErrorBoundary';
 import Root from '@/components/Root';
+import Auction from '@/pages/auction';
 import CatCollection from '@/pages/catCollection';
 import Contest from '@/pages/contest';
 import Gacha from '@/pages/gacha';
 import Login from '@/pages/login';
 import NotFoundPage from '@/pages/notFound';
+import ProfilePage from '@/pages/profile';
 import SalePage from '@/pages/sale';
 import Service from '@/pages/service';
 import Signup from '@/pages/signup';
@@ -20,6 +22,10 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <Service />,
+      },
+      {
+        path: 'profile',
+        element: <ProfilePage />,
       },
       {
         path: 'sale',
@@ -44,6 +50,10 @@ export const router = createBrowserRouter([
       {
         path: 'gallery',
         element: <CatCollection />,
+      },
+      {
+        path: 'auction',
+        element: <Auction />,
       },
     ],
   },
