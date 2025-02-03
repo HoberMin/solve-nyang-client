8828afda58e051058e871569b2748b47898eb842
ebeleey <ebeleey@gmail.com>
2025-02-03 23:18:04 +0900

    design: 판매 내역 탭 표 수정

--- src/pages/auction/components/AuctionHistory.tsx
+++ src/pages/auction/components/AuctionHistory.tsx
@@ -59,6 +59,12 @@ const statusConfig = {
   cancelled: { color: 'text-gray-400', text: '취소됨' },
 };
 
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+
+  return `${date.getFullYear()}.${String(date.getMonth() + 1).padStart(2, '0')}.${String(date.getDate()).padStart(2, '0')}`;
+};
+
 const AuctionHistory = () => {
   const [filter, setFilter] = useState<FilterType>(0);
   const [currentPage, setCurrentPage] = useState(1);
@@ -67,18 +73,13 @@ const AuctionHistory = () => {
     filter: filter === 0 ? undefined : filter,
     page: currentPage,
   });
+
   const { mutate: cancelAuctionItem } = useCancelAuctionItem();
 
   const getStatus = (item: { sold: boolean; cancelled: boolean }) => {
     if (item.sold) return 'completed';
     if (item.cancelled) return 'cancelled';
     return 'inProgress';
-  };
-
-  const formatDate = (dateString: string) => {
-    const date = new Date(dateString);
-
-    return `${date.getFullYear()}년 ${String(date.getMonth() + 1).padStart(2, '0')}월 ${String(date.getDate()).padStart(2, '0')}일`;
   };
 
   const handleCancel = (id: number) => {
@@ -96,7 +97,7 @@ const AuctionHistory = () => {
           value={filter.toString()}
           onValueChange={value => setFilter(Number(value) as FilterType)}
         >
-          <SelectTrigger className='h-12 w-56 border-transparent bg-gray-800 p-4 text-gray-200'>
+          <SelectTrigger className='h-12 w-56 border-transparent bg-gray-700 text-gray-200'>
             <SelectValue placeholder='상태 필터' />
           </SelectTrigger>
           <SelectContent className='border-transparent bg-gray-800 text-gray-200'>
@@ -107,15 +108,16 @@ const AuctionHistory = () => {
           </SelectContent>
         </Select>
       </div>
+
       <div className='rounded-lg bg-gray-800'>
         <Table>
           <TableHeader>
             <TableRow className='rounded-lg border-gray-700 hover:bg-transparent'>
               <TableHead className='text-center text-gray-200'></TableHead>
-              <TableHead className='text-center text-gray-200'>이름</TableHead>
               <TableHead className='text-center text-gray-200'>등급</TableHead>
+              <TableHead className='text-center text-gray-200'>이름</TableHead>
               <TableHead className='text-center text-gray-200'>가격</TableHead>
-              <TableHead className='w-[150px] text-center text-gray-200'>
+              <TableHead className='text-center text-gray-200'>
                 등록일
               </TableHead>
               <TableHead className='text-center text-gray-200'>상태</TableHead>
@@ -128,24 +130,24 @@ const AuctionHistory = () => {
                 key={item.id}
                 className='border-gray-600 text-base hover:bg-transparent'
               >
-                <TableCell className='w-24'>
+                <TableCell className='w-32'>
                   <div className='flex justify-center'>
                     <img
                       src={`/cats/${item.name}.svg`}
                       alt={item.name}
-                      className='h-16 w-16 object-cover pl-2'
+                      className='h-16 w-16 object-cover'
                     />
                   </div>
-                </TableCell>
-                <TableCell className='text-center font-medium text-gray-200'>
-                  {getCatKorName(item.name)}
                 </TableCell>
                 <TableCell className='text-center'>
                   <span
                     className={cn('font-bold', rarityConfig[item.rarity]?.text)}
                   >
-                    {item.rarity}등급
+                    {item.rarity}
                   </span>
+                </TableCell>
+                <TableCell className='text-center text-gray-200'>
+                  {getCatKorName(item.name)}
                 </TableCell>
                 <TableCell className='text-center font-bold text-blue-400'>
                   {item.price.toLocaleString()}냥
@@ -161,45 +163,47 @@ const AuctionHistory = () => {
                 >
                   {statusConfig[getStatus(item)].text}
                 </TableCell>
-                <TableCell className='w-[120px]'>
-                  {getStatus(item) === 'inProgress' && (
-                    <AlertDialog>
-                      <AlertDialogTrigger asChild>
-                        <Button className='h-8 bg-gray-600 px-4 text-sm text-gray-200 hover:bg-gray-500'>
-                          취소하기
-                        </Button>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent className='border-transparent bg-gray-800 text-gray-200'>
-                        <AlertDialogHeader>
-                          <AlertDialogTitle>
-                            <p className='text-center text-2xl'>취소 확인</p>
-                          </AlertDialogTitle>
-                          <AlertDialogDescription className='text-center text-base text-gray-200'>
-                            <span
-                              className={cn(
-                                'font-bold',
-                                rarityConfig[item.rarity]?.text,
-                              )}
+                <TableCell className='w-36'>
+                  <div className='flex justify-center'>
+                    {getStatus(item) === 'inProgress' && (
+                      <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                          <Button className='bg-blue-500 hover:bg-blue-600'>
+                            취소하기
+                          </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent className='border-transparent bg-gray-800 text-gray-400'>
+                          <AlertDialogHeader>
+                            <AlertDialogTitle>
+                              <p className='text-center text-2xl'>취소 확인</p>
+                            </AlertDialogTitle>
+                            <AlertDialogDescription className='text-center text-base text-gray-200'>
+                              <span
+                                className={cn(
+                                  'font-bold',
+                                  rarityConfig[item.rarity]?.text,
+                                )}
+                              >
+                                {getCatKorName(item.name)}
+                              </span>
+                              을(를) 다시 데려올까요?
+                            </AlertDialogDescription>
+                          </AlertDialogHeader>
+                          <AlertDialogFooter>
+                            <AlertDialogCancel className='bg-gray-700 text-gray-200 hover:bg-gray-600'>
+                              취소
+                            </AlertDialogCancel>
+                            <AlertDialogAction
+                              onClick={() => handleCancel(item.id)}
+                              className='bg-blue-500 hover:bg-blue-600'
                             >
-                              {getCatKorName(item.name)}
-                            </span>
-                            을(를) 다시 데려올까요?
-                          </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                          <AlertDialogAction
-                            className='bg-gray-600 text-gray-200 hover:bg-gray-500'
-                            onClick={() => handleCancel(item.id)}
-                          >
-                            확인
-                          </AlertDialogAction>
-                          <AlertDialogCancel className='bg-gray-700 text-gray-200 hover:bg-gray-600'>
-                            취소
-                          </AlertDialogCancel>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialog>
-                  )}
+                              확인
+                            </AlertDialogAction>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    )}
+                  </div>
                 </TableCell>
               </TableRow>
             ))}
