778fe8f16bd1520a8c829fdfb6489fc7f16f5d45
ebeleey <ebeleey@gmail.com>
2025-02-04 17:10:21 +0900

    design: 배경 상점 헤더

--- src/pages/background/index.tsx
+++ src/pages/background/index.tsx
@@ -142,19 +142,34 @@ const BackgroundShop = () => {
 
   return (
     <Layout>
-      <div className='mx-auto max-w-7xl px-4 py-12'>
-        <div className='absolute right-8 top-20'>
-          <PointDisplay point={userPoint} />
+      <div className='relative mx-auto mb-8 flex h-full w-full max-w-7xl flex-col'>
+        <div className='sticky top-0 z-10 flex bg-gray-900/95 px-6 py-4 backdrop-blur-sm'>
+          <div className='flex-1'>
+            <div className='mb-2 mt-8 text-2xl font-bold text-blue-400 shadow-blue-400/50 drop-shadow-lg'>
+              배경 상점
+            </div>
+            <p className='text-lg text-gray-400'>
+              냥코인으로 배경 이미지를 구매하여 나만의 이미지를 개성있게
+              꾸며보세요.
+            </p>
+            <p className='mt-5 text-base text-gray-400'>
+              * 구매 후 환불이나 유저 간 거래는 불가합니다.
+            </p>
+          </div>
+          <div className='my-8'>
+            <PointDisplay point={userPoint} />
+          </div>
         </div>
-        <h1 className='mb-12 text-3xl font-bold text-white'>배경 상점</h1>
-        <div className='grid grid-cols-1 gap-12 md:grid-cols-2'>
-          {backgrounds?.map(background => (
-            <BackgroundCard
-              key={background.name}
-              background={background}
-              userPoint={userPoint}
-            />
-          ))}
+        <div className='mx-auto max-w-7xl px-4 py-12'>
+          <div className='grid grid-cols-1 gap-12 md:grid-cols-2'>
+            {backgrounds?.map(background => (
+              <BackgroundCard
+                key={background.name}
+                background={background}
+                userPoint={userPoint}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
