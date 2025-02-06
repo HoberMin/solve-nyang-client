606f5376e95720f26336a497b4fa2748f6efc33e
zoe <ddu_buk_e@naver.com>
2025-02-07 02:12:08 +0900

    feat: 출석체크 컴포넌트 수정

---
+++ src/pages/checkAttendance/componenets/TodaysProblem.tsx
@@ -0,0 +1,14 @@
+const TodaysProblem = () => {
+  return (
+    <>
+      <div className='text-white'>
+        <div>오늘의 문제</div>
+        <p>[17136] : 색종이 붙이기</p>
+        <button className='font-medium text-blue-500 transition-colors hover:text-blue-400'>
+          문제풀기
+        </button>
+      </div>
+    </>
+  );
+};
+export default TodaysProblem;
--- src/pages/checkAttendance/index.tsx
+++ src/pages/checkAttendance/index.tsx
@@ -1,14 +1,29 @@
 import Layout from '@/components/Layout';
 
+import PointDisplay from '../gacha/components/PointDisplay';
 import AttendanceCalendar from './componenets/AttendanceCalendar';
 import AttendanceStamp from './componenets/AttendanceStamp';
+import TodaysProblem from './componenets/TodaysProblem';
 
 const CheckAttendance = () => {
   return (
     <>
       <Layout>
-        <AttendanceStamp />
-        <AttendanceCalendar />
+        <div>
+          <PointDisplay />
+          <div className='text-center text-white'>
+            <div className='text-3xl'>출석체크하고 냥코인 받자!</div>
+            <p className='text-xl'>월~금 연속 출석 시 추가포인트 지급</p>
+            <p className='text-xl'>한달 개근 시시 추가포인트 지급</p>
+          </div>
+          <div>
+            <AttendanceStamp />
+            <AttendanceCalendar />
+          </div>
+          <div>
+            <TodaysProblem />
+          </div>
+        </div>
       </Layout>
     </>
   );
