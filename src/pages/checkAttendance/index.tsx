cd239c096ce42afe082dd041a5a12fa7e3964ee9
Zoe <ddu_buk_e@naver.com>
2025-02-07 09:53:15 +0900

    design: ui 임시 수정

--- src/pages/checkAttendance/index.tsx
+++ src/pages/checkAttendance/index.tsx
@@ -9,12 +9,14 @@ const CheckAttendance = () => {
   return (
     <>
       <Layout>
-        <div>
-          <PointDisplay />
-          <div className='text-center text-white'>
-            <div className='text-3xl'>출석체크하고 냥코인 받자!</div>
-            <p className='text-xl'>월~금 연속 출석 시 추가포인트 지급</p>
-            <p className='text-xl'>한달 개근 시시 추가포인트 지급</p>
+        <div className='text-center text-white'>
+          <div className='text-3xl'>출석체크하고 냥코인 받자!</div>
+          <p className='text-xl'>월~금 연속 출석 시 추가포인트 지급</p>
+          <p className='text-xl'>한달 개근 시 추가포인트 지급</p>
+        </div>
+        <div className='flex'>
+          <div>
+            <PointDisplay />
           </div>
           <div>
             <AttendanceStamp />
