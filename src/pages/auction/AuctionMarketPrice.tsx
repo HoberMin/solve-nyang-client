6744525180b4dd66bd624bd76fdb7489e95cf5af
ebeleey <ebeleey@gmail.com>
2025-02-07 11:18:03 +0900

    feat: 시세 탭 UI

--- src/pages/auction/AuctionMarketPrice.tsx
+++ src/pages/auction/AuctionMarketPrice.tsx
@@ -1,7 +1,158 @@
+import { useState } from 'react';
+
+import { ArrowUpDown } from 'lucide-react';
+
+import { SortType, useGetAuctionList } from '@/apis/auction';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { RARITY_CONFIG } from '@/constant/rarityconfig';
+import { RarityFilterType } from '@/lib/type';
+import { cn, formatDate, getCatKorName } from '@/lib/utils';
+
+import CustomPagination from './components/CustomPagination';
+import FilterSidebar from './components/FilterSidebar';
+
 const AuctionMarketPrice = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortBy, setSortBy] = useState<SortType>(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedRarity, setSelectedRarity] = useState<RarityFilterType>('ALL');
+  const [inputValue, setInputValue] = useState('');
+
+  const queryParams = {
+    keyword: searchTerm || undefined,
+    rarity: selectedRarity === 'ALL' ? undefined : selectedRarity,
+    sort: sortBy,
+    page: currentPage,
+  };
+
+  const { data } = useGetAuctionList(queryParams);
+  const { totalPage, merchandises } = data || {};
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearchTerm(inputValue);
+    setCurrentPage(1);
+  };
+
+  const handleRarity = (value: RarityFilterType) => {
+    setSelectedRarity(value);
+    setCurrentPage(1);
+  };
+
+  const handleSortPrice = () => {
+    setSortBy(current => {
+      if (current === 0 || current === 2) return 1;
+      return 2;
+    });
+    setCurrentPage(1);
+  };
+
+  const handleSortDate = () => {
+    setSortBy(() => {
+      return 0;
+    });
+    setCurrentPage(1);
+  };
+
   return (
-    <div>
-      <h1 className='text-white'>시세 탭</h1>
+    <div className='flex gap-6'>
+      <FilterSidebar
+        inputValue={inputValue}
+        selectedRarity={selectedRarity}
+        onInputChange={handleChange}
+        onSearch={handleSearch}
+        onRarityChange={handleRarity}
+      />
+
+      <div className='flex-1'>
+        <div className='rounded-lg bg-gray-800'>
+          <Table>
+            <TableHeader>
+              <TableRow className='rounded-lg border-gray-700 hover:bg-transparent'>
+                <TableHead className='w-32 text-center text-gray-200'></TableHead>
+                <TableHead className='w-12 text-center text-gray-200'>
+                  등급
+                </TableHead>
+                <TableHead className='w-40 text-center text-gray-200'>
+                  이름
+                </TableHead>
+                <TableHead
+                  className='w-44 cursor-pointer text-center text-gray-200'
+                  onClick={handleSortPrice}
+                >
+                  <div className='flex items-center justify-center gap-2'>
+                    가격
+                    <ArrowUpDown className='h-4 w-4' />
+                  </div>
+                </TableHead>
+                <TableHead
+                  className='w-40 cursor-pointer text-center text-gray-200'
+                  onClick={handleSortDate}
+                >
+                  <div className='flex items-center justify-center gap-2'>
+                    등록일
+                    <ArrowUpDown className='h-4 w-4' />
+                  </div>
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody className='rounded-lg bg-gray-700'>
+              {merchandises?.map(item => (
+                <TableRow
+                  key={item.id}
+                  className='border-gray-600 text-base hover:bg-transparent'
+                >
+                  <TableCell className='w-32'>
+                    <div className='flex justify-center'>
+                      <img
+                        src={`/cats/${item.name}.svg`}
+                        alt={item.name}
+                        className='h-16 w-16 object-cover'
+                      />
+                    </div>
+                  </TableCell>
+                  <TableCell className='text-center'>
+                    <span
+                      className={cn(
+                        'font-bold',
+                        RARITY_CONFIG[item.rarity]?.text,
+                      )}
+                    >
+                      {item.rarity}
+                    </span>
+                  </TableCell>
+                  <TableCell className='text-center text-gray-200'>
+                    {getCatKorName(item.name)}
+                  </TableCell>
+                  <TableCell className='text-center font-bold text-blue-400'>
+                    {item.price.toLocaleString()}냥
+                  </TableCell>
+                  <TableCell className='text-center text-sm text-gray-300'>
+                    {formatDate(item.createdAt)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+
+          <CustomPagination
+            currentPage={currentPage}
+            totalPage={totalPage}
+            onPageChange={setCurrentPage}
+          />
+        </div>
+      </div>
     </div>
   );
 };
