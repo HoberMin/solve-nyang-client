e646413c7227526fdea97c065c5ae542d5c86978
ebeleey <ebeleey@gmail.com>
2025-02-08 16:02:26 +0900

    refactor: 판매 내역 탭 컴포넌트 분리

--- src/pages/auction/AuctionHistory.tsx
+++ src/pages/auction/AuctionHistory.tsx
@@ -7,18 +7,6 @@ import {
   useGetUserAuctionList,
 } from '@/apis/auction';
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
-import { Button } from '@/components/ui/button';
-import {
   Table,
   TableBody,
   TableCell,
@@ -29,6 +17,7 @@ import {
 import { RARITY_CONFIG } from '@/constant/rarityconfig';
 import { cn, formatDate, getCatKorName } from '@/lib/utils';
 
+import CancelDialog from './components/CancelDialog';
 import CustomPagination from './components/CustomPagination';
 import HistorySidebar from './components/HistoryFilter';
 
@@ -140,42 +129,7 @@ const AuctionHistory = () => {
                 <TableCell className='w-36'>
                   <div className='flex justify-center'>
                     {getStatus(item) === 'inProgress' && (
-                      <AlertDialog>
-                        <AlertDialogTrigger asChild>
-                          <Button className='bg-blue-500 hover:bg-blue-600'>
-                            취소하기
-                          </Button>
-                        </AlertDialogTrigger>
-                        <AlertDialogContent className='border-transparent bg-gray-800 text-gray-400'>
-                          <AlertDialogHeader>
-                            <AlertDialogTitle>
-                              <p className='text-center text-2xl'>취소 확인</p>
-                            </AlertDialogTitle>
-                            <AlertDialogDescription className='text-center text-base text-gray-200'>
-                              <span
-                                className={cn(
-                                  'font-bold',
-                                  RARITY_CONFIG[item.rarity].text,
-                                )}
-                              >
-                                {getCatKorName(item.name)}
-                              </span>
-                              을(를) 다시 데려올까요?
-                            </AlertDialogDescription>
-                          </AlertDialogHeader>
-                          <AlertDialogFooter>
-                            <AlertDialogAction
-                              onClick={() => handleCancel(item.id)}
-                              className='bg-blue-500 hover:bg-blue-600'
-                            >
-                              확인
-                            </AlertDialogAction>
-                            <AlertDialogCancel className='bg-gray-700 text-gray-200 hover:bg-gray-600'>
-                              취소
-                            </AlertDialogCancel>
-                          </AlertDialogFooter>
-                        </AlertDialogContent>
-                      </AlertDialog>
+                      <CancelDialog item={item} onCancel={handleCancel} />
                     )}
                   </div>
                 </TableCell>
