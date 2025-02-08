f373639e6bfa0fa49d33995b2c8f3169a5859508
zoe <ddu_buk_e@naver.com>
2025-02-08 20:55:02 +0900

    design: 오늘의 문제 ui 수정

--- src/pages/checkAttendance/componenets/TodaysProblem.tsx
+++ src/pages/checkAttendance/componenets/TodaysProblem.tsx
@@ -1,12 +1,20 @@
 const TodaysProblem = () => {
   return (
     <>
-      <div className='text-center text-white'>
-        <div>오늘의 문제</div>
-        <p>[17136] : 색종이 붙이기</p>
-        <button className='font-medium text-blue-500 transition-colors hover:text-blue-400'>
-          문제풀기
-        </button>
+      <div className='flex items-center justify-center'>
+        <div className='space-y-5 text-center text-white'>
+          <div className='text-2xl'>오늘의 문제</div>
+          <p>17136</p>
+          <div className='flex flex-col space-y-4'>
+            <button className='font-medium text-blue-500 transition-colors hover:text-blue-400'>
+              문제풀기
+            </button>
+
+            <button className='font-medium text-blue-500 transition-colors hover:text-blue-400'>
+              냥코인 받기
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/pages/checkAttendance/index.tsx
+++ src/pages/checkAttendance/index.tsx
@@ -9,11 +9,10 @@ const CheckAttendance = () => {
   return (
     <>
       <Layout>
-        <div className='flex justify-center'>
-          <div className='text-center text-white'>
-            <div className='text-3xl'>출석체크하고 냥코인 받자!</div>
-            <p className='text-xl'>월~금 연속 출석 시 추가포인트 지급</p>
-            <p className='text-xl'>한달 개근 시 추가포인트 지급</p>
+        <div className='mt-20 flex justify-center gap-8'>
+          <div className='space-y-3 text-center text-white'>
+            <div className='text-3xl'>오늘의 문제 풀고 냥코인 받자!</div>
+            <p className='text-xl'>월~일 연속 출석 시 추가 포인트 지급</p>
           </div>
           <PointDisplay />
         </div>
