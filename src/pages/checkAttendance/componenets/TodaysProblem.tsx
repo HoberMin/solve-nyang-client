0fd3ffa260560a63c762d29cc493fd335ceb7d71
zoe <ddu_buk_e@naver.com>
2025-02-09 13:59:10 +0900

    feat: 중간 커밋

--- src/pages/checkAttendance/componenets/TodaysProblem.tsx
+++ src/pages/checkAttendance/componenets/TodaysProblem.tsx
@@ -1,10 +1,16 @@
+// import { useGetProblem } from '@/apis/check';
+
 const TodaysProblem = () => {
+  // const { data } = useGetProblem();
+  // const { problemId } = data;
+
   return (
     <>
       <div className='flex items-center justify-center'>
         <div className='space-y-5 text-center text-white'>
-          <div className='text-2xl'>오늘의 문제</div>
+          <div className='text-2xl'>솔브냥이 추천하는 오늘의 문제</div>
           <p>17136</p>
+          {/* <p>{problemId}</p> */}
           <div className='flex flex-col space-y-4'>
             <button className='font-medium text-blue-500 transition-colors hover:text-blue-400'>
               문제풀기
--- src/pages/checkAttendance/index.tsx
+++ src/pages/checkAttendance/index.tsx
@@ -11,8 +11,8 @@ const CheckAttendance = () => {
       <Layout>
         <div className='mt-20 flex justify-center gap-8'>
           <div className='space-y-3 text-center text-white'>
-            <div className='text-3xl'>오늘의 문제 풀고 냥코인 받자!</div>
-            <p className='text-xl'>월~일 연속 출석 시 추가 포인트 지급</p>
+            <div className='text-3xl'>알고리즘 풀고 냥코인 받자!</div>
+            <p className='text-xl'>월 ~ 일 연속 풀이 시 추가 포인트 지급</p>
           </div>
           <PointDisplay />
         </div>
