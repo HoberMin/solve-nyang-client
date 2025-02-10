4ad7d4669d5a97e65786b7f57c1197f92c67e07b
Zoe <ddu_buk_e@naver.com>
2025-02-10 14:20:19 +0900

    design: ui 수정

--- src/pages/checkAttendance/index.tsx
+++ src/pages/checkAttendance/index.tsx
@@ -12,6 +12,9 @@ const CheckAttendance = () => {
           <div className='space-y-3 text-center text-white'>
             <div className='text-3xl'>알고리즘 풀고 냥코인 받자!</div>
             <p className='text-xl'>월 ~ 일 연속 풀이 시 추가 포인트 지급</p>
+            <p className='text-sm'>
+              * 본인 랭킹에 적합한 문제를 풀어야 포인트 지급
+            </p>
           </div>
           <PointDisplay />
         </div>
